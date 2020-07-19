e16fe331099f86a4624a6d60a8af2efd0526803b
jmenjivar <jmenjivar@lifemiles.loc>
2020-07-18 18:50:03 -0600

    dsw: por eliminacion de todo los *.spec

--- src/app/Pipes/noimg.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NoimgPipe } from './noimg.pipe';
-
-describe('NoimgPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NoimgPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
